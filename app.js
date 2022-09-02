369ed7e58c72f26c673f218522278125d563b957
govind15496 <govindsingh15496@gmail.com>
2022-09-02 13:28:08 +0530

    changes02

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.post("/failure.html", function(req,res){
 });
 
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("Server is running on port 3000");
 });
 
